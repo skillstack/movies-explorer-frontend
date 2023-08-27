875cf35930b2595b03176a78f2a1de89d6b0b9fd
Vitaly Tsyganov <format911@yandex.ru>
2023-08-27 09:03:24 +0300

    fix

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://backend.nomoredomains.work";
+export const BASE_URL = "https://api.vitaliydiploma.nomoredomainsicu.ru";
 
 function getResponse(res) {
   if (!res.ok) {
